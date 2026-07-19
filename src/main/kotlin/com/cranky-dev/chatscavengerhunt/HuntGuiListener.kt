a7783dbaea6c6b3afedc77f6591c16a73b869335
Cranky-dev <130955083+Cranky-dev@users.noreply.github.com>
2026-07-18 22:07:34 -0500

    Create HuntGuiListener.kt

---
+++ src/main/kotlin/com/cranky-dev/chatscavengerhunt/HuntGuiListener.kt
@@ -0,0 +1,38 @@
+package com.cranky-dev.chatscavengerhunt
+
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+
+class HuntGuiListener(private val plugin: ScavengerHunt) : Listener {
+
+    @EventHandler
+    fun onInventoryClick(event: InventoryClickEvent) {
+        if (event.view.title() != plugin.color("&8Hunt Control Panel")) return
+        
+        event.isCancelled = true // Lock the GUI items down
+        
+        val player = event.whoClicked as? Player ?: return
+        val clickedItem = event.currentItem ?: return
+
+        when (clickedItem.type) {
+            Material.GREEN_CONCRETE -> {
+                player.closeInventory()
+                plugin.conversationFactory.buildConversation(player).begin()
+            }
+            Material.RED_CONCRETE -> {
+                player.closeInventory()
+                if (!plugin.isHuntActive) {
+                    player.sendMessage(plugin.color("&cThere is no running hunt to stop."))
+                    return
+                }
+                plugin.stopHuntManually()
+                Bukkit.broadcast(plugin.color("&4&l[SERVER] &cThe Scavenger Hunt has been stopped by an administrator."))
+            }
+            else -> {}
+        }
+    }
+}
